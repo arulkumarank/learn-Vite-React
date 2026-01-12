ac0fbf0701dec23800a07e1efb364785e2951037
arulkumarank <arulkumaran187@gmail.com>
2026-01-12 20:10:04 +0530

    useRef — Access DOM & Store Values Without Re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
-import { useEffect, useState } from 'react'
+import {useRef, useEffect, useState } from 'react'
 import Child from "./components/Child.jsx"
 
 function App() {
 
+  const inputRef = useRef(null);
+  const renderCount = useRef(0);
   const [count, setCount] = useState(0);
+
+  renderCount.current+=1;
+  console.log("App Rendered", renderCount.current);
 
   useEffect(()=>{
     console.log("Use Effect Executed");
@@ -13,14 +18,18 @@ function App() {
       console.log("cleanup Executed");
     };
   },[]);
-  
-  console.log("App Rendered");
+
   
   return (
     
     <div>
-      <h1>Lifting State Up</h1>
-      <h1>Parent Count : {count}</h1>
+      <h1>UseRef Demo</h1>
+
+      <input style={{padding:'8px'}} ref= {inputRef} placeholder='Type something'/>
+      <button onClick={()=>inputRef.current.focus()}>Focus input</button>
+
+
+      <h1>State Count : {count}</h1>
       <Child count = {count} setCount = {setCount}/>
     </div>
   );
